527bda3155bb7aa47776442354b45f7ca33f3ed1
Dracnor <antoine.domenech@gmail.com>
2018-04-28 11:35:42 +0200

    correction du programme 3 : j'avais inversé les rôles des sorties

--- exercice3/programme_3.c
+++ exercice3/programme_3.c
@@ -8,17 +8,17 @@ int main(int argc, char const *argv[]) {
         /* On fixe l'amplitude à 1 et la fréquence à 10 */
         /* Le temps incrémente de 1 à chaque nouveau complexe envoyé */
         /* Donc l'argument en 2*pi*f*t est toujours multiple de 2*pi */
-        /** Donc : si le bit est pair, on envoie 1.0, 0.0 (z = 1) **/
-        /** Sinon, on envoie 0.0, -1.0 (z = -i) **/
+        /** Donc : si le bit est pair, on envoie 0.0, -1.0 (z = -i)  **/
+        /** Sinon, on envoie  1.0, 0.0 (z = 1) **/
 
         int ascii = 1;
 
         while ( (ascii = getchar()) && ascii != EOF) {
                 if (ascii == '0' ) {
                         /* le ascii de 0 est pair, celui de 1 impair */
-                        printf("%f\n%f\n", 1.0, 0.0);
-                } else if (ascii == '1') {
                         printf("%f\n%f\n", 0.0, -1.0);
+                } else if (ascii == '1') {
+                        printf("%f\n%f\n", 1.0, 0.0);
                 }
                 /* else : ignore */
         }
