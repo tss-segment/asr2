4b6d7093998bccf0c85186ba23029a98f25d8741
Dracnor <antoine.domenech@gmail.com>
2018-04-28 11:32:30 +0200

    Programme 3

--- exercice3/programme_3.c
+++ exercice3/programme_3.c
@@ -6,8 +6,21 @@
 
 int main(int argc, char const *argv[]) {
         /* On fixe l'amplitude à 1 et la fréquence à 10 */
-        /* Comme dans l'exemple, on prendra t = 0 */
-        /* Ce qui fait qu'on a simplement 0 -> (1.0, 0.0) */
-        /*                                1 -> (-1.0, 0.0) */
+        /* Le temps incrémente de 1 à chaque nouveau complexe envoyé */
+        /* Donc l'argument en 2*pi*f*t est toujours multiple de 2*pi */
+        /** Donc : si le bit est pair, on envoie 1.0, 0.0 (z = 1) **/
+        /** Sinon, on envoie 0.0, -1.0 (z = -i) **/
+
+        int ascii = 1;
+
+        while ( (ascii = getchar()) && ascii != EOF) {
+                if (ascii == '0' ) {
+                        /* le ascii de 0 est pair, celui de 1 impair */
+                        printf("%f\n%f\n", 1.0, 0.0);
+                } else if (ascii == '1') {
+                        printf("%f\n%f\n", 0.0, -1.0);
+                }
+                /* else : ignore */
+        }
         return 0;
 }
