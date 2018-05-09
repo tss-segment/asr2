251487aa756fc285f73c8231aad9bfdb2f4c5aba
Black Rook <sebastien.mld@numericable.fr>
2018-05-09 13:57:16 +0200

    Slight fix for programe 6: you must output 0/1, not -1/1

--- exercice3/programme_6.c
+++ exercice3/programme_6.c
@@ -7,13 +7,13 @@
 int main(int argc, char const *argv[]) {
         /* lis l'entree avec fscanf, et renvoie un signal débruité */
 
-        float a, b;
+        int a, b;
 
-        while ( (fscanf(stdin, "%f\n%f\n", &a, &b) != EOF) ) {
-                if (a==1.0 && b==0.0) {
+        while ( (fscanf(stdin, "%d\n%d\n", &a, &b) != EOF) ) {
+                if (a==1 && b==0) {
                         printf("%i", 1);
-                } else if (a==-1.0 && b==0.0) {
-                        printf("%i\n", -1);
+                } else if (a==-1 && b==0) {
+                        printf("%i\n", 0);
                 } else {
                         fprintf(stderr, "ERROR_WHILE_PARSING_BPSK");
                 }
